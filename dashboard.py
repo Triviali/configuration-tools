c6390d347d8a1e94bda836e2549cc59419a50866
Triviali <152082840+Triviali@users.noreply.github.com>
2023-11-26 12:43:10 -0500

    Comment Out Sidebar
    Sidebar is deprecated due to WMS update.
    Sidebar also breaks on Streamlit Cloud, so
    it has been commented out for cloud compatability.

--- dashboard.py
+++ dashboard.py
@@ -84,68 +84,68 @@ dfGpumem = get_data_from_excel_6()
 
 
 # Sidebar
-st.sidebar.header("Enter your second-page options here.")
+# st.sidebar.header("Enter your second-page options here.")
 
-adapter = st.sidebar.selectbox(
-    "Select Adapter",
-    options=dfFilter["adapterIncluded"].unique(),
-)
+# adapter = st.sidebar.selectbox(
+#     "Select Adapter",
+#     options=dfFilter["adapterIncluded"].unique(),
+# )
 
-screen = st.sidebar.selectbox(
-    "Select LCD Condition",
-    options=dfFilter["screenCondition"].unique(),
+# screen = st.sidebar.selectbox(
+#     "Select LCD Condition",
+#     options=dfFilter["screenCondition"].unique(),
 
-)
+# )
 
-keyboard = st.sidebar.selectbox(
-    "Select Keyboard Condition",
-    options=dfFilter["keyboardCondition"].unique(),
-)
+# keyboard = st.sidebar.selectbox(
+#     "Select Keyboard Condition",
+#     options=dfFilter["keyboardCondition"].unique(),
+# )
 
-os = st.sidebar.selectbox(
-    "Select Operating System",
-    options=dfFilter["operatingSystem"].unique(),
-)
+# os = st.sidebar.selectbox(
+#     "Select Operating System",
+#     options=dfFilter["operatingSystem"].unique(),
+# )
 
-battery_condition = st.sidebar.selectbox(
-    "Select Battery Condition",
-    options=dfFilter["battCond"].unique(),
-)
+# battery_condition = st.sidebar.selectbox(
+#     "Select Battery Condition",
+#     options=dfFilter["battCond"].unique(),
+# )
 
-cycle_count = st.sidebar.number_input(
-    "Select Cycle Count",
-    min_value=0,
-    max_value=1000,
-)
+# cycle_count = st.sidebar.number_input(
+#     "Select Cycle Count",
+#     min_value=0,
+#     max_value=1000,
+# )
 
-gpu_make = st.sidebar.selectbox(
-    "Select GPU Make",
-    options=dfGpumake["gpuMake"].unique(),
+# gpu_make = st.sidebar.selectbox(
+#     "Select GPU Make",
+#     options=dfGpumake["gpuMake"].unique(),
+# 
+# )
 
-)
+# gpu_model = st.sidebar.selectbox(
+#     "Select GPU Model",
+#     options=dfGpumodel["gpuModel"].unique(),
 
-gpu_model = st.sidebar.selectbox(
-    "Select GPU Model",
-    options=dfGpumodel["gpuModel"].unique(),
+# )
 
-)
+# gpu_mem = st.sidebar.selectbox(
+#     "Select GPU Memory",
+#     options=dfGpumem["gpuMem"].unique(),
 
-gpu_mem = st.sidebar.selectbox(
-    "Select GPU Memory",
-    options=dfGpumem["gpuMem"].unique(),
-
-)
-text_search = st.text_input("Type your entry in the box below to search for a configuration.", value='')
-lenovo_copy = st.sidebar.button("Copy 2nd Page for Lenovo Configuration")
-apple_copy = st.sidebar.button("Copy 2nd Page for Apple Configuraton")
-if lenovo_copy == True:
-    pc.copy(adapter + '\n' + screen + '\n' + keyboard + '\n' + os + '\n' + gpu_make + '\n' + gpu_model + '\n' + gpu_mem)
-    lenovo_copy == False
-    st.toast("Copied to clipboard!")
-if apple_copy == True:
-    pc.copy(adapter + '\n' + screen + '\n' + keyboard + '\n' + os + '\n' + battery_condition + '\n' + str(cycle_count) + '\n' + gpu_make)
-    apple_copy == False
-    st.toast("Copied to clipboard!")
+# )
+# text_search = st.text_input("Type your entry in the box below to search for a configuration.", value='')
+# lenovo_copy = st.sidebar.button("Copy 2nd Page for Lenovo Configuration")
+# apple_copy = st.sidebar.button("Copy 2nd Page for Apple Configuraton")
+# if lenovo_copy == True:
+#     pc.copy(adapter + '\n' + screen + '\n' + keyboard + '\n' + os + '\n' + gpu_make + '\n' + gpu_model + '\n' + gpu_mem)
+ #    lenovo_copy == False
+#     st.toast("Copied to clipboard!")
+# if apple_copy == True:
+#     pc.copy(adapter + '\n' + screen + '\n' + keyboard + '\n' + os + '\n' + battery_condition + '\n' + str(cycle_count) + '\n' + gpu_make)
+#     apple_copy == False
+#     st.toast("Copied to clipboard!")
 
 dfFilter_selection = dfFilter.query(
     "adapterIncluded == @adapter & screenCondition == @screen & keyboardCondition == @keyboard & operatingSystem == @os & battCond == @battery_condition"
